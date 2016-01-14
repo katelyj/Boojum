e3bc90996c4a4f6d15dcb672071b9ce41be9f974
Bayless <bsmithsalzberg@stuy.edu>
2016-01-13 21:50:21 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -12,7 +12,7 @@ public class Player {
     private String name;
     private int yourCase;
     private double luck;
-    private double likeability;
+    private double likability;
     private InputStreamReader isr;
     private BufferedReader in;
 
@@ -42,6 +42,33 @@ public class Player {
 	}
 	return n;
     }
+   // gets and returns mode from the user
+    public String inputMode() {
+	int n = 0;
+	while ( n == 0 ) { // to ensure non-empty names
+	    System.out.println("Please type 1 if you would like to be a regular player" +
+	    "\n Please type 2 if you would like to be a likable player, but unlucky" +
+	    "\n Please type 3 if you would like to be a lucky player, but not likable");
+	    try {
+		n = in.parseInt(in.readLine());
+	    }
+	    catch ( IOException e ) {}
+	}
+	
+	if (n == 2){
+		return "Congrats, you are a regular person!";
+	}
+	if (n == 2){
+		setLikability((getLikablility() + 1));
+		return "Congrats, you are a likable person!";
+	}
+	if (n == 3){
+		setLuck(getLuck() + .1);
+		return "Congrats, you are a lucky person!"
+	else {
+		return "please specify your mode";
+	}
+	    }
 
     // accessor for name
     public String getName() {
@@ -54,8 +81,14 @@ public class Player {
     }
 
     // accessor for likeability
-    public double getLikeability() {
+    public double getLikability() {
 	return likeability;
+    }
+    public void setLikability(double i){
+    	likability = i;
+    }
+    public void setLuck(double i){
+    	luck = i;
     }
 
     // mutator for yourCase
