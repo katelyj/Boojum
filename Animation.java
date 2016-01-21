fa1e1ad4cff148f91558ccbf49cf9b580dd8ef23
Bayless <bsmithsalzberg@stuy.edu>
2016-01-21 17:24:55 -0500

    does this move for you???

--- Animation.java
+++ Animation.java
@@ -3,9 +3,27 @@ public class Animation{
     public Animation(String p){
 	person = p;
     }
+    public void waits() {
+	try {
+	    Thread.sleep(300); // 1000 milliseconds is one second
+	} catch(InterruptedException ex) {
+	    Thread.currentThread().interrupt();
+	}
+    }
     public void dance(){
 	if (person.equals("wave")){
+	    System.out.println("\033c");
 	    System.out.println("          (oo)\n  +========\\/\n / || %%% ||\n*  ||-----||\n   \"\"     \"\"\n");
+	    waits();
+	    System.out.println("\033c");
+	    System.out.println("          (oo)\n  +========\\/\n/ || %%% ||\n*  ||-----||\n   \"\"     \"\"\n");
+	    waits();
+	    System.out.println("\033c");
+	    System.out.println("          (oo)\n _+========\\/\n   || %%% ||\n*  ||-----||\n   \"\"     \"\"\n");
+	    waits();
+	    System.out.println("\033c");
+	    System.out.println("          (oo)\n \\+========\\/\n   || %%% ||\n*  ||-----||\n   \"\"     \"\"\n");
+	    
 	}
     }
 }
